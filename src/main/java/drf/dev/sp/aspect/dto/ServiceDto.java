0191109277feab324b5291cb256b2d42a070907f
drfdev <drafff@gmail.com>
2020-01-02 00:47:03 +0300

    some fixes

--- src/main/java/drf/dev/sp/aspect/dto/ServiceDto.java
+++ src/main/java/drf/dev/sp/aspect/dto/ServiceDto.java
@@ -1,8 +1,10 @@
 package drf.dev.sp.aspect.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class ServiceDto {
     private final String name;
     private String description;
--- src/test/java/drf/dev/sp/aspect/dto/ServiceDtoTest.java
+++ src/test/java/drf/dev/sp/aspect/dto/ServiceDtoTest.java
@@ -5,14 +5,23 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ServiceDtoTest {
-    private ServiceDto dto = new ServiceDto("TestName");
-
     @Test
     public void testToString() {
-        dto.setDescription("TestDescription");
-        dto.setValue(10);
+        ServiceDto dto = new ServiceDto("TestName", "TestDescription", 10);
 
         assertEquals("ServiceDto(name=TestName, description=TestDescription, value=10)",
+                dto.toString());
+    }
+
+    @Test
+    public void testBuilder() {
+        ServiceDto dto = ServiceDto.builder()
+                .name("BuildName")
+                .description("BuildDescription")
+                .value(12)
+                .build();
+
+        assertEquals("ServiceDto(name=BuildName, description=BuildDescription, value=12)",
                 dto.toString());
     }
 }
