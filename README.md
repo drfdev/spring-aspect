49ebe8ad20300d786f1d92127acd0fe087cd785e
drfdev <drafff@gmail.com>
2020-01-01 20:25:37 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+### Spring-aspect
+
+Spring boot + Spring aspect demo
+
+Java version: 13
+
+Spring boot version: 2.2.2.RELEASE
---
+++ src/test/java/drf/dev/sp/aspect/SpringAspectApplicationTests.java
@@ -0,0 +1,12 @@
+package drf.dev.sp.aspect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringAspectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
